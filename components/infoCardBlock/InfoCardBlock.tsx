f0ea2bd68e3c53e08c8dcc75696436d5508a43c6
ShadePrima <aleksandrag1984@gmail.com>
2023-10-30 20:52:43 +0200

    hot fix info card

--- components/infoCardBlock/InfoCardBlock.tsx
+++ components/infoCardBlock/InfoCardBlock.tsx
@@ -1,14 +1,11 @@
 import InfoCard from '@/ui/infoCard/InfoCard'
-
 import routes from '@/utils/routes'
 import { Locale } from '@/i18n-config'
-
 import homeGreen from '../../assets/icons/home-green.svg'
 import industry from '../../assets/icons/industry.svg'
 import anvil from '../../assets/icons/anvil.svg'
 import worker from '../../assets/icons/worker.svg'
 import window from '../../assets/icons/window.svg'
-
 import styles from './InfoCardBlok.module.scss'
 
 type Props = {
@@ -28,7 +25,7 @@ export default function InfoCardBlock({ dictionary, lang }: Props) {
     },
     {
       id: 2,
-      icon: industry,
+      icon: worker,
       title: dictionary.infoCard.cardTwo.title,
       text: dictionary.infoCard.cardTwo.text,
       link: routes.equipmentCard,
@@ -45,14 +42,14 @@ export default function InfoCardBlock({ dictionary, lang }: Props) {
     {
       id: 4,
       icon: industry,
-      title: dictionary.infoCard.cardTwo.title,
-      text: dictionary.infoCard.cardTwo.text,
+      title: dictionary.infoCard.cardFourth.title,
+      text: dictionary.infoCard.cardFourth.text,
       link: routes.equipCard,
       buttonTitle: dictionary.infoCard.infoButton,
     },
     {
       id: 5,
-      icon: window,
+      icon: industry,
       title: dictionary.infoCard.cardFifth.title,
       text: dictionary.infoCard.cardFifth.text,
       link: routes.windowCard,
@@ -60,7 +57,7 @@ export default function InfoCardBlock({ dictionary, lang }: Props) {
     },
     {
       id: 6,
-      icon: worker,
+      icon: window,
       title: dictionary.infoCard.cardSixth.title,
       text: dictionary.infoCard.cardSixth.text,
       link: routes.generalCard,
