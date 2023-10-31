a9de3a5644e8261402e8a99c16039a1a7bb52819
ShadePrima <aleksandrag1984@gmail.com>
2023-10-31 13:07:09 +0200

    hot fix infor card and add translate

--- app/[lang]/equip/page.tsx
+++ app/[lang]/equip/page.tsx
@@ -1,27 +1,101 @@
-import { Locale } from '@/i18n-config'
+import Image from 'next/image'
+import equip from '../../../assets/image/equip.png'
+import image2 from '../../../assets/image/build-image-2.png'
+import image3 from '../../../assets/image/build-image-3.png'
+import image4 from '../../../assets/image/build-image-4.png'
+import { Locale, i18n } from '@/i18n-config'
+import { getDictionary } from '@/get-dictionary'
+import { Inter } from 'next/font/google'
+const inter = Inter({ subsets: ['latin'] })
+import Link from 'next/link'
 import styles from './page.module.scss'
 
 export const metadata = {
-  title: 'Продаж промислового обладнання',
+  title: 'Виготовлення будинків з морських контейнерів',
   description: 'CREATIFF',
 }
 
-type Props = {
+export async function generateStaticParams() {
+  return i18n.locales.map((locale) => ({ lang: locale }))
+}
+
+type BuildProps = {
   params: { lang: Locale }
 }
 
-export default function Equipment({ params }: Props) {
-  console.log(params.lang, 'equipmentPage')
+export default async function Equip({ params }: BuildProps) {
+  const dictionary = await getDictionary(params.lang)
+
   return (
-    <div className={styles.root}>
+    <section className={styles.build}>
       <div className='container'>
-        {/* <div className={styles.image}>
-          <Image src={image1} alt='build' />
-        </div> */}
-        <div className={styles.textBlock}>
-          <h1>Продаж промислового обладнання</h1>
+        <div className={styles.build__image}>
+          <Image src={equip} width={800} height={600} alt='build' />
+        </div>
+        <div className={styles.build__textBlock}>
+          <h1 className={`${styles.build__title} ${inter.className}`}>
+            {dictionary.build.sectionOne.titleOne}
+          </h1>
+
+          <p className={styles.build__text}>
+            {dictionary.build.sectionOne.paragraphOne}
+          </p>
+
+          <p className={styles.build__text}>
+            {dictionary.build.sectionOne.paragraphTwo}
+          </p>
+
+          <p className={styles.build__text}>
+            {dictionary.build.sectionOne.paragraphThree}
+          </p>
+
+          <p className={styles.build__text}>
+            {dictionary.build.sectionOne.paragraphFour}
+          </p>
+
+          <p className={styles.build__text}>
+            {dictionary.build.sectionOne.paragraphFive_1}
+            <Link
+              className={`${styles.build__link} ${'link'}`}
+              target='_blank'
+              href={
+                'https://www.casasprefabricadas.mobi/normativa-y-legislacion-sobre-las-casas-prefabricadas-en-espana/?fbclid=IwAR24v0ohZYCW-RgI5cn_A2KQXg89kM0zzULslVYxo262VFbApB2X2x8d1A8'
+              }
+            >
+              {dictionary.build.sectionOne.link_five}
+            </Link>
+            {dictionary.build.sectionOne.paragraphFive_2}
+          </p>
+
+          <p className={styles.build__text}>
+            {dictionary.build.sectionOne.paragraphSix}
+            <Link
+              className={`${styles.build__link} ${'link'}`}
+              target='_blank'
+              href={
+                'http://www.casasprefabricadas24.com/quiero-una-casa-prefabricada-que-documentacion-necesito/?fbclid=IwAR1SfsD7dq1OXswkpW9eFi8WQqC_VmWGDDq28s0w9cFbe1qHITxQolCJ040'
+              }
+            >
+              {dictionary.build.sectionOne.link_six}
+            </Link>
+          </p>
+
+          <p className={styles.build__text}>
+            {dictionary.build.sectionOne.paragraphSeven}
+          </p>
+        </div>
+        <div className={styles.build__image}>
+          <Image src={image2} alt='build' />
+        </div>
+
+        <div className={styles.build__image}>
+          <Image src={image3} alt='build' />
+        </div>
+
+        <div className={styles.build__image}>
+          <Image src={image4} alt='build' />
         </div>
       </div>
-    </div>
+    </section>
   )
 }
