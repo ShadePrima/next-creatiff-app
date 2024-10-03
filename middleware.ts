f6ffeda0cdb41b49b4dc73fc0cf3d8c8431f4961
Ross <komarostyslav@gmail.com>
2024-10-03 16:27:26 +0000

    middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -54,6 +54,11 @@ export function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
+  matcher: [
+    '/((?!api/|_next/).*)',
+    '!/favicon.ico',
+    '!/robots.txt',
+    '!/sitemap.xml',
+  ],
 }
 
