c7aa54ba106b2aea218c1d296b5f4cb42db678bd
Aleks <aleksandrag1984@gmail.com>
2024-10-07 20:24:07 +0300

    hot fix article 5

--- app/[lang]/article-5/page.tsx
+++ app/[lang]/article-5/page.tsx
@@ -45,120 +45,121 @@ export default async function Metal({ params }: BuildProps) {
 
   return (
     <section className={styles.article_5}>
-      <div className='container_article'>
-        <BreadCrumb model={items} home={home} />
-        <div className='title_main'>{dict.article_5.title_main}</div>
-        <div className='title'>{dict.article_5.title_1}</div>
-        <div className='text pt_32'>{dict.article_5.text_1}</div>
-        <ol className='list'>
-            <li className='pt_8'>{dict.article_5.text_list_1}</li>
-            <li>{dict.article_5.text_list_2}</li>
-            <li>{dict.article_5.text_list_3}</li>
-        </ol>
-        <div className='text pt_8'>{dict.article_5.text_2}</div>
-        <div className={styles.article_5__wrapper}>
-          <div className={styles.article_5__image_block}>
-            <Image src={image_1} alt='image 1' />
-            <Image src={image_2} alt='image 2' />
-            <div className='text'>{dict.article_5.text_2_4}</div>
-          </div>
-          <div className={styles.article_5__text_block}>
-            <div className='text_bold pt_0'>{dict.article_5.title_2}</div>
-            <div className='text pt_8'>{dict.article_5.text_2_1}</div>
-            <div className='text pt_8'>{dict.article_5.text_2_2}</div>
-            <div className='text pt_8'>{dict.article_5.text_2_3}</div>
-          </div>
+        <div className='container_article'>
+            <BreadCrumb model={items} home={home}/>
+            <div className='title_main'>{dict.article_5.title_main}</div>
+            <div className='title'>{dict.article_5.title_1}</div>
+            <div className='text pt_32'>{dict.article_5.text_1}</div>
+            <ol className='list'>
+                <li className='pt_8'>{dict.article_5.text_list_1}</li>
+                <li>{dict.article_5.text_list_2}</li>
+                <li>{dict.article_5.text_list_3}</li>
+            </ol>
+            <div className='text pt_8'>{dict.article_5.text_2}</div>
+            <div className={styles.article_5__wrapper}>
+                <div className={styles.article_5__image_block}>
+                    <Image src={image_1} alt='image 1'/>
+                    <Image src={image_2} alt='image 2'/>
+                    <div className='text'>{dict.article_5.text_2_4}</div>
+                </div>
+                <div className={styles.article_5__text_block}>
+                    <div className='text_bold pt_0'>{dict.article_5.title_2}</div>
+                    <div className='text pt_8'>{dict.article_5.text_2_1}</div>
+                    <div className='text pt_8'>{dict.article_5.text_2_2}</div>
+                    <div className='text pt_8'>{dict.article_5.text_2_3}</div>
+                </div>
+            </div>
+            <div className={styles.article_5__wrapper}>
+                <div className={styles.article_5__image_block}>
+                    <Image src={image_3} alt='image 3'/>
+                </div>
+                <div className={styles.article_5__text_block}>
+                    <div className='text_bold pt_0'>{dict.article_5.title_3}</div>
+                    <div className='text pt_8'>{dict.article_5.text_3_1}</div>
+                    <div className='text pt_8'>{dict.article_5.text_3_2}</div>
+                </div>
+            </div>
+            <div className={styles.article_5__wrapper}>
+                <div className={styles.article_5__image_block}>
+                    <Image src={image_4} alt='image 4'/>
+                </div>
+                <div className={styles.article_5__text_block}>
+                    <div className='text_bold pt_0'>{dict.article_5.title_4}</div>
+                    <div className='text pt_8'>{dict.article_5.text_4_1}</div>
+                    <div className='text pt_8'>{dict.article_5.text_4_2}</div>
+                </div>
+            </div>
+            <div className={styles.article_5__wrapper}>
+                <div className={styles.article_5__image_block}>
+                    <Image src={image_5} alt='image 5'/>
+                </div>
+                <div className={styles.article_5__text_block}>
+                    <div className='text_bold pt_0'>{dict.article_5.title_5}</div>
+                    <div className='text pt_8'>{dict.article_5.text_5_1}</div>
+                    <div className='text pt_8'>{dict.article_5.text_5_2}</div>
+                </div>
+            </div>
+            <div className={styles.article_5__wrapper}>
+                <div className={styles.article_5__image_block}>
+                    <Image src={image_6} alt='image 6'/>
+                </div>
+                <div className={styles.article_5__text_block}>
+                    <div className='text_bold pt_0'>{dict.article_5.title_6}</div>
+                    <div className='text pt_8'>{dict.article_5.text_6_1}</div>
+                    <div className='text pt_8'>{dict.article_5.text_6_2}</div>
+                </div>
+            </div>
+            <div className={styles.article_5__wrapper}>
+                <div className={styles.article_5__image_block}>
+                    <Image src={image_7} alt='image 7'/>
+                </div>
+                <div className={styles.article_5__text_block}>
+                    <div className='text_bold pt_0'>{dict.article_5.title_7}</div>
+                    <div className='text pt_8'>{dict.article_5.text_7_1}</div>
+                </div>
+            </div>
+            <div className={styles.article_5__wrapper}>
+                <div className={styles.article_5__image_block}>
+                    <Image src={image_8} alt='image 8'/>
+                </div>
+                <div className={styles.article_5__text_block}>
+                    <div className='text_bold pt_0'>{dict.article_5.title_8}</div>
+                    <div className='text pt_8'>{dict.article_5.text_8_1}</div>
+                    <div className='text pt_8'>{dict.article_5.text_8_2}</div>
+                    <div className='text pt_8'>{dict.article_5.text_8_3}</div>
+                </div>
+            </div>
+            <div className={styles.article_5__wrapper}>
+                <div className={styles.article_5__image_block}>
+                    <Image src={image_9} alt='image 9'/>
+                </div>
+                <div className={styles.article_5__text_block}>
+                    <div className='text_bold pt_0'>{dict.article_5.title_9}</div>
+                    <div className='text pt_8'>{dict.article_5.text_9_1}</div>
+                    <div className='text pt_8'>{dict.article_5.text_9_2}</div>
+                </div>
+            </div>
+            <div className='title_main'>{dict.article_5.title_10}</div>
+            <div className='title'>{dict.article_5.title_11}</div>
+            <div className='text pt_16'>{dict.article_5.text_11_1}</div>
+            <div className='text pt_8'>{dict.article_5.text_11_2}</div>
+            <div className='text pt_8'>{dict.article_5.text_11_3}</div>
+            <Image className='pt_72' src={image_10} alt='image 10'/>
+            <div className={styles.article_5__wrapper}>
+                <div className={styles.article_5__image_block}>
+                    <Image src={image_11} alt='image 11'/>
+                    <div className='text'>{dict.article_5.text_11_4}</div>
+                </div>
+                <div className={styles.article_5__text_block}>
+                    <ul className='list_dot'>
+                        <li className='pt_16'>{dict.article_5.text_list_11_1}</li>
+                        <li>{dict.article_5.text_list_11_2}</li>
+                        <Image src={image_12} alt='image 12'/>
+                    </ul>
+                </div>
+            </div>
+            <div className='text pt_16'>{dict.article_5.text_11_5}</div>
         </div>
-        <div className={styles.article_5__wrapper}>
-          <div className={styles.article_5__image_block}>
-            <Image src={image_3} alt='image 3' />
-          </div>
-          <div className={styles.article_5__text_block}>
-            <div className='text_bold pt_0'>{dict.article_5.title_3}</div>
-            <div className='text pt_8'>{dict.article_5.text_3_1}</div>
-            <div className='text pt_8'>{dict.article_5.text_3_2}</div>
-          </div>
-        </div>
-        <div className={styles.article_5__wrapper}>
-          <div className={styles.article_5__image_block}>
-            <Image src={image_4} alt='image 4' />
-          </div>
-          <div className={styles.article_5__text_block}>
-            <div className='text_bold pt_0'>{dict.article_5.title_4}</div>
-            <div className='text pt_8'>{dict.article_5.text_4_1}</div>
-            <div className='text pt_8'>{dict.article_5.text_4_2}</div>
-          </div>
-        </div>
-        <div className={styles.article_5__wrapper}>
-          <div className={styles.article_5__image_block}>
-            <Image src={image_5} alt='image 5' />
-          </div>
-          <div className={styles.article_5__text_block}>
-            <div className='text_bold pt_0'>{dict.article_5.title_5}</div>
-            <div className='text pt_8'>{dict.article_5.text_5_1}</div>
-            <div className='text pt_8'>{dict.article_5.text_5_2}</div>
-          </div>
-        </div>
-        <div className={styles.article_5__wrapper}>
-            <div className={styles.article_5__image_block}>
-                <Image src={image_6} alt='image 6' />
-            </div>
-            <div className={styles.article_5__text_block}>
-                <div className='text_bold pt_0'>{dict.article_5.title_6}</div>
-                <div className='text pt_8'>{dict.article_5.text_6_1}</div>
-                <div className='text pt_8'>{dict.article_5.text_6_2}</div>
-            </div>
-        </div>
-        <div className={styles.article_5__wrapper}>
-            <div className={styles.article_5__image_block}>
-                <Image src={image_7} alt='image 7' />
-            </div>
-            <div className={styles.article_5__text_block}>
-                <div className='text_bold pt_0'>{dict.article_5.title_7}</div>
-                <div className='text pt_8'>{dict.article_5.text_7_1}</div>
-            </div>
-        </div>
-        <div className={styles.article_5__wrapper}>
-            <div className={styles.article_5__image_block}>
-                <Image src={image_8} alt='image 8' />
-            </div>
-            <div className={styles.article_5__text_block}>
-                <div className='text_bold pt_0'>{dict.article_5.title_8}</div>
-                <div className='text pt_8'>{dict.article_5.text_8_1}</div>
-                <div className='text pt_8'>{dict.article_5.text_8_2}</div>
-                <div className='text pt_8'>{dict.article_5.text_8_3}</div>
-            </div>
-        </div>
-        <div className={styles.article_5__wrapper}>
-            <div className={styles.article_5__image_block}>
-                <Image src={image_9} alt='image 9' />
-            </div>
-            <div className={styles.article_5__text_block}>
-                <div className='text_bold pt_0'>{dict.article_5.title_9}</div>
-                <div className='text pt_8'>{dict.article_5.text_9_1}</div>
-                <div className='text pt_8'>{dict.article_5.text_9_2}</div>
-            </div>
-        </div> 
-        <div className='title_main'>{dict.article_5.title_10}</div>
-        <div className='title'>{dict.article_5.title_11}</div>
-        <div className='text pt_16'>{dict.article_5.text_11_1}</div>
-        <div className='text pt_8'>{dict.article_5.text_11_2}</div>
-        <div className='text pt_8'>{dict.article_5.text_11_3}</div>
-        <Image className='pt_72' src={image_10} alt='image 10' />
-        <div className={styles.article_5__wrapper}>
-            <div className={styles.article_5__image_block}>
-                <Image src={image_11} alt='image 11' />
-                <div className='text'>{dict.article_5.text_11_4}</div>
-            </div>
-            <div className={styles.article_5__text_block}>
-            <ul className='list_dot'>
-                <li className='pt_16'>{dict.article_5.text_list_11_1}</li>
-                <li>{dict.article_5.text_list_11_2}</li>
-                <Image src={image_12} alt='image 12' />
-            </ul>
-            </div>
-        </div>
-      </div>
     </section>
   )
 }
